7e6befee4c3a286ef76267561e99f097b4ea97f1
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-24 15:56:49 +0000

    learning about templates

--- study_django/blog/views.py
+++ study_django/blog/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+"""trying with dummy data"""
+
+posts = [
+    {
+        'author': 'HabieM',
+        'title':'Blog Post 1',
+        'content': 'First post on here!',
+        'date_posted': 'May 5, 2021'
+    },
+    {
+        'author': 'MansiBoo',
+        'title': 'Blog Post 2',
+        'content': 'I was dragged here not by my will',
+        'date_posted': 'May 6, 2021'
+    },
+    {
+        'author': 'Babo',
+        'title': 'Blog Post 3',
+        'content': 'Looks basicc!',
+        'date_posted': 'May 6, 2021'
+    },
+    {
+        'author': 'Amor',
+        'title': 'Blog Post 4',
+        'content': 'Que sera, sera',
+        'date_posted': 'May 7, 2021'
+    }
+]
 
 """Function presenting the blog home page"""
 def blog_home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    """dictionary with our dummy data as key to give access to home template"""
+    context_key = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context_key)
 
 """Function presenting the blog about page"""
 def blog_about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
